66140a236000f91001612e8c0f3bc8e79b1259ee
Ben Fischer <benfischer@outlook.com>
2022-02-18 23:22:03 -0500

    Init
    Initial Commit

---
+++ dhcp.py
@@ -0,0 +1,25 @@
+import os
+import routeros_api
+
+try:
+  con = routeros_api.RouterOsApiPool(
+    'router.contoso.com',
+    username='admin',
+    password='B@dPassword1!',
+    use_ssl=True,
+    ssl_verify=False,
+    plaintext_login=True
+  )
+  dhcp = api.get_resource('/ip/dhcp-server/lease').get()
+except:
+  print("FAILURE")
+  os._exit(1)
+print("Address,MAC-Address,Hostname,Last-Seen,Expires-After")
+for rec in dhcp:
+  try:
+    hostname = rec['host-name']
+  except KeyError:
+    hostname = ""
+  print(f"{rec['address']},{rec['mac-address']},{hostname},{rec['last-seen']},{rec['expires-after']}")
+con.disconnect()
+os._exit(0)
